23612f2aead04e6a529bce8035af021b03d0f6fd
Julien Gil <julien.gil@fifty-five.com>
2018-03-16 11:14:04 +0100

    updateFramework.sh is a script that helps to update a framework within the projects that depend on it :  rm, rebuild and cp. Rebuild the frameworks based on the one just updated

--- updateFramework.sh
+++ updateFramework.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+SCRIPT_NAME="updateFramework.sh"
 
-APP_path="./CargoTestApp/Frameworks/"
+APP_path="./CargoTestApp/"
+APP_path_frmwrks="${APP_path}Frameworks/"
 
 CAR="./CargoCore/"
 CARFr="CargoCore.framework"
 
 rebuild() {
 	cd $1
-	echo "Rebuild the framework for the $1 directory..."
+	echo "${SCRIPT_NAME}: Rebuild the framework for the $1 directory..."
 	xcodebuild -target "Aggregate"
-	cd ..
+	cd -
 }
 
 updateCargo() {
@@ -18,46 +20,47 @@ updateCargo() {
 	FB_path="./FacebookHandler/"
 	TUN_path="./TuneHandler/"
 
-	AT_path_frmwrks="$AT_pathFrameworks/"
-	ADB_path_frmwrks="$ADB_pathFrameworks/"
-	FB_path_frmwrks="$FB_pathFrameworks/"
-	TUN_path_frmwrks="$TUN_pathFrameworks/"
+	AT_path_frmwrks="${AT_path}Frameworks/"
+	ADB_path_frmwrks="${ADB_path}Frameworks/"
+	FB_path_frmwrks="${FB_path}Frameworks/"
+	TUN_path_frmwrks="${TUN_path}Frameworks/"
 
-	rm -rf "$AT_path_frmwrks$CARFr"
-	rm -rf "$ADB_path_frmwrks$CARFr"
-	rm -rf "$APP_path$CARFr"
-	rm -rf "$FB_path_frmwrks$CARFr"
-	rm -rf "$TUN_path_frmwrks$CARFr"
+	rm -rf "${AT_path_frmwrks}${CARFr}"
+	rm -rf "${ADB_path_frmwrks}${CARFr}"
+	rm -rf "${APP_path_frmwrks}${CARFr}"
+	rm -rf "${FB_path_frmwrks}${CARFr}"
+	rm -rf "${TUN_path_frmwrks}${CARFr}"
 
-	cp -R "$CAR$CARFr" "$AT_path_frmwrks"
-	cp -R "$CAR$CARFr" "$ADB_path_frmwrks"
-	cp -R "$CAR$CARFr" "$APP_path"
-	cp -R "$CAR$CARFr" "$FB_path_frmwrks"
-	cp -R "$CAR$CARFr" "$TUN_path_frmrwks"
+	cp -R "${CAR}${CARFr}" "${AT_path_frmwrks}"
+	cp -R "${CAR}${CARFr}" "${ADB_path_frmwrks}"
+	cp -R "${CAR}${CARFr}" "${APP_path_frmwrks}"
+	cp -R "${CAR}${CARFr}" "${FB_path_frmwrks}"
+	cp -R "${CAR}${CARFr}" "${TUN_path_frmwrks}"
 
-	rebuild $AT_path
-	rebuild $ADB_path
-	rebuild $FB_path
-	rebuild $TUN_path
+	rebuild ${AT_path}
+	rebuild ${ADB_path}
+	rebuild ${FB_path}
+	rebuild ${TUN_path}
 }
 
 error() {
-	echo "Possible arguments : 'ATInternet', 'Adobe', 'Facebook', 'CargoCore' or 'Tune'"
+	echo "${SCRIPT_NAME}: Possible arguments are 'ATInternet', 'Adobe', 'Facebook', 'CargoCore' or 'Tune'"
 	exit
 }
 
-if [$# == 0 || [$# == 1 && $1 == "CargoCore"]]; then
+if [[ $# == 0 || ($# == 1 && $1 == "CargoCore") ]]; then
 	updateCargo
-elif [$# > 1]; then
-	echo "Too many arguments. Either use one or none."
+elif [[ $# > 1 ]]; then
+	echo "${SCRIPT_NAME}: Too many arguments. Either use one or none."
 	error
-elif [$# = 1 && [$1 == "ATInternet" || $1 == "Adobe" || $1 == "Facebook" || $1 == "Tune"]]; then
-	handler="$1Handler"
-	framework="$handler.framework"
-	APP_path_frmwrk="$APP_path$framework"
+elif [[ $# == 1 && ($1 == "ATInternet" || $1 == "Adobe" || $1 == "Facebook" || $1 == "Tune") ]]; then
+	handler="${1}Handler"
+	framework="${handler}.framework"
 
-	rm -rf "$APP_path_frmwrk"
-	cp -R "./$handler/$framework" "$APP_path"
+	rm -rf "${APP_path_frmwrks}${framework}"
+	cp -R "./${handler}/${framework}" "${APP_path_frmwrks}"
+
+	echo "${SCRIPT_NAME}: Copied ${framework} into ${APP_path_frmwrks}"
 else
 	error
 fi
